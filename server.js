7b828bf5313cf713b2a4d8c043819e78ab7aaf8e
Akash Raman <akashjayaraman8841@gmail.com>
2023-03-09 22:00:07 +0530

    update

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.post('/user/register', async (req,res) => {
 app.post('/user', async (req,res) => {
   console.log(req.body);
   const token = `${req.body.token}`;
-  const user = await Users.findOne({ first_name:"Akash" });
+  const user = await Users.findOne({ '_id': token });
   return res.send(user);
 })
  
